e6f52a693cdb68ed97d762b5bfab7b36d0e5c905
LionelVanS <lionelvs@outlook.fr>
2023-03-19 09:45:55 +0100

    adding footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export default function Footer() {
+   return (
+      <StyledFooter>
+         <StyledP>
+            © 2023 - Lionel Van Schellebeck - Développeur Web - Javascript |
+            React / NextJS.
+         </StyledP>
+      </StyledFooter>
+   );
+}
+
+// STYLES
+const StyledFooter = styled.footer`
+   position: absolute;
+   bottom: 0;
+   left: 0;
+   right: 0;
+`;
+const StyledP = styled.p`
+   text-align: center;
+`;
