03f607af01b19aaf4beff19d11fcd02093ebe457
LionelVanS <lionelvs@outlook.fr>
2023-03-19 09:43:17 +0100

    adding global style, header & footer. Removing css stylesheet

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,54 @@
-import '@/styles/globals.css'
+import { createGlobalStyle } from 'styled-components';
 
+// IMPORTED UTILS
+import colors from '@/utils/style/colors';
+
+// IMPORTED COMPONENTS
+import Footer from '@/components/Footer/footer';
+import Header from '@/components/Header/Header';
+
+// EXPORTED COMPONENT
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+   return (
+      <>
+         <GlobalStyle />
+         <Header />
+         <Component {...pageProps} />
+         <Footer />
+      </>
+   );
 }
+
+// STYLES
+const GlobalStyle = createGlobalStyle`
+      html,
+      body {
+          padding:0;
+          margin:0;
+          background-color: ${colors.background};
+          font-family: 'Amazon Ember', sans-serif;
+      }
+
+      * {
+         box-sizing: border-box;
+      }
+
+      li {
+          list-style-type: none;
+      }
+
+      a {
+         color: white;
+         text-decoration: none;
+      }
+      p {
+         color: white;
+      }
+
+      ul {
+         margin:0;
+         padding:0;
+      }
+
+      
+    `;
