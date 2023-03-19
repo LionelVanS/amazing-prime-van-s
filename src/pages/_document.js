58bb4a4b834ee51b48ca7cd76030227e1d732281
LionelVanS <lionelvs@outlook.fr>
2023-03-19 09:43:57 +0100

    adding font's stylesheet

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+   return (
+      <Html lang="fr">
+         <Head>
+            <link
+               href="https://fonts.cdnfonts.com/css/amazon-ember"
+               rel="stylesheet"
+            />
+         </Head>
+         <body>
+            <Main />
+            <NextScript />
+         </body>
+      </Html>
+   );
 }
