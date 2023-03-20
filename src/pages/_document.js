80923d5d35bbd31a2908e10e2b8c2452f46fb393
LionelVanS <lionelvs@outlook.fr>
2023-03-20 13:48:34 +0100

    adding link and meta to project

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,15 @@ export default function Document() {
                href="https://fonts.cdnfonts.com/css/amazon-ember"
                rel="stylesheet"
             />
+            <link rel="shortcut icon" href="./assets/images/favicon.png" />
+            <meta
+               name="description"
+               content="Bienvenue sur mon portfolio. Il s'agit d'un clône du site prime vidéo."
+            />
+            <meta
+               name="viewport"
+               content="width=device-width, initial-scale=1"
+            />
          </Head>
          <body>
             <Main />
