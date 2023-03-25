1299bd6a3554c70c2ee3d3e50a0138ef03751531
LionelVanS <lionelvs@outlook.fr>
2023-03-25 16:46:04 +0100

    adding hero banner's data

---
+++ src/utils/data/heroBannerData.js
@@ -0,0 +1,39 @@
+// IMPORT STATIC IMAGE
+import homeBackground from '../../../public/assets/images/homepage-background.jpg';
+import careerBackground from '../../../public/assets/images/careerpage-background.jpg';
+import skillsBackground from '../../../public/assets/images/skillpage-background.jpg';
+import portfolioBackground from '../../../public/assets/images/portfoliopage-background.jpg';
+
+// EXPORTED HOME PAGE DATA
+export const homePageData = {
+   title: 'Lionel',
+   secondPartTitle: 'VAN SCHELLEBECK',
+   subtitle: 'A Brand New Front-End Developer !',
+   technologies: 'Javascript | React',
+   image: homeBackground,
+   link: '/profil',
+};
+
+// EXPORTED CAREER PAGE DATA
+export const careerPageData = {
+   title: 'Mon Parcours',
+   subtitle: "C'est quoi qu'est ce que j'ai fait de ma vie !",
+   image: careerBackground,
+   link: '/',
+};
+
+// EXPORTED SKILLS PAGE DATA
+export const skillsPageData = {
+   title: 'Mes Skills',
+   subtitle: 'Les technos que je maitrise',
+   image: skillsBackground,
+   link: '/',
+};
+
+// EXPORTED PORTFOLIO PAGE DATA
+export const portfolioPageData = {
+   title: 'Lionel VAN SCHELLEBECK',
+   subtitle: "Le développeur qu'il vous faut",
+   image: portfolioBackground,
+   link: '/',
+};
