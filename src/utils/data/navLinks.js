03287588beb07ca65586342713799cf76b7cb5d6
LionelVanS <lionelvs@outlook.fr>
2023-03-25 16:52:04 +0100

    adding links

---
+++ src/utils/data/navLinks.js
@@ -0,0 +1,44 @@
+export const navLinks = [
+   {
+      name: 'Accueil',
+      path: '/',
+      subNav: [
+         {
+            name: 'Profil',
+            path: '/profil',
+         },
+      ],
+   },
+   {
+      name: 'Parcours',
+      path: '/career',
+      subNav: [
+         {
+            name: 'Expériences',
+            path: '/career',
+         },
+         {
+            name: 'Formations',
+            path: '/career',
+         },
+      ],
+   },
+   {
+      name: 'Compétences',
+      path: '/skills',
+      subNav: [
+         {
+            name: 'Techniques',
+            path: '/skills',
+         },
+         {
+            name: 'Transversales',
+            path: '/skills',
+         },
+      ],
+   },
+   {
+      name: 'Portfolio',
+      path: '/portfolio',
+   },
+];
