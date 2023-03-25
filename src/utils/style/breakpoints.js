d10435b7b83aa44b926977319b46867e1f709b5a
LionelVanS <lionelvs@outlook.fr>
2023-03-25 16:52:12 +0100

    adding breakpoints

---
+++ src/utils/style/breakpoints.js
@@ -0,0 +1,9 @@
+const sizes = {
+   mobile: '880px',
+   tablet: '1400px',
+};
+
+export const devices = {
+   mobile: `(max-width: ${sizes.mobile})`,
+   tablet: `(max-width: ${sizes.tablet})`,
+};
