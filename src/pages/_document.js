8ba27070142e18a49042b9732544642cbb25b084
LionelVanS <lionelvs@outlook.fr>
2023-03-26 14:58:58 +0200

    removing font's call and meta viewport

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,18 +4,10 @@ export default function Document() {
    return (
       <Html lang="fr">
          <Head>
-            <link
-               href="https://fonts.cdnfonts.com/css/amazon-ember"
-               rel="stylesheet"
-            />
-            <link rel="shortcut icon" href="./assets/images/favicon.png" />
+            <link rel="shortcut icon" href="./images/favicon.png" />
             <meta
                name="description"
                content="Bienvenue sur mon portfolio. Il s'agit d'un clône du site prime vidéo."
-            />
-            <meta
-               name="viewport"
-               content="width=device-width, initial-scale=1"
             />
          </Head>
          <body>
