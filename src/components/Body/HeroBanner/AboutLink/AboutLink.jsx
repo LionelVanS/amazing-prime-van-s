e62e394116efd5cc83e26ed2a4c26bc598e29992
LionelVanS <lionelvs@outlook.fr>
2023-03-31 19:07:56 +0200

    removing svg's styles

--- src/components/Body/HeroBanner/AboutLink/AboutLink.jsx
+++ src/components/Body/HeroBanner/AboutLink/AboutLink.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import styled from 'styled-components';
 
 // IMPORTED COMPONENTS
-import PlayIcon from '../../Icons/PlayIcon';
+import PlayIcon from '../../../Icons/PlayIcon';
 
 // EXPORTED COMPONENT
 export default function AboutLink({ link }) {
@@ -20,12 +20,6 @@ export default function AboutLink({ link }) {
 const AboutLinkDiv = styled.div`
    a {
       display: flex;
-      svg {
-         width: 5vw;
-         transition: transform 75ms linear;
-         :hover {
-            transform: scale(1.1);
-         }
       }
       p {
          font-size: 2vw;
