baea4a674b7b95a8ee08614d7f24a669186c0f5c
LionelVanS <lionelvs@outlook.fr>
2023-04-07 13:43:18 +0200

    removing files

--- src/components/Body/SmallCards/BackgroundImage/BackgroundImage.jsx
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image';
-
-export default function BackgroundImage({ data }) {
-   return (
-      <Image src={data.background} alt={data.title} width="500" height="768" />
-   );
-}
