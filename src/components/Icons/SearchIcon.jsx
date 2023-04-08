ac20479d04913469962217289e38f3efe1aa973a
LionelVanS <lionelvs@outlook.fr>
2023-04-08 14:31:06 +0200

    updating size and position

--- src/components/Icons/SearchIcon.jsx
+++ src/components/Icons/SearchIcon.jsx
@@ -6,14 +6,13 @@ export default function SearchIcon() {
          <SearchIconDiv>
             <svg
                xmlns="http://www.w3.org/2000/svg"
-               fill=""
                viewBox="0 0 24 24"
                strokeWidth="2.5"
                stroke="#aaaaaa"
                strokeLinecap="round"
             >
-               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-               <circle cx="10" cy="10" r="7" />
+               <path stroke="none" fill="none" d="M0 0h24v24H0z" />
+               <circle cx="10" cy="10" r="7" fill="none" />
                <line x1="21" y1="21" x2="15" y2="15" />
             </svg>
          </SearchIconDiv>
@@ -27,12 +26,12 @@ const SearchIconDiv = styled.div`
    display: flex;
    align-items: center;
    justify-content: center;
-   margin: 0 1vw 0 4vw;
+   margin-left: 4vw;
    cursor: pointer;
    :hover {
       background-color: #191e25;
    }
    svg {
-      width: 2vw;
+      width: 1.5vw;
    }
 `;
