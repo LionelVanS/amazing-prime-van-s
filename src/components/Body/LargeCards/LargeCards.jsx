958472c08df32084bd31977c7b57bc8799169346
LionelVanS <lionelvs@outlook.fr>
2023-04-08 18:53:59 +0200

    Adding LargeCards

--- src/components/Body/LargeCards/LargeCards.jsx
+++ src/components/Body/LargeCards/LargeCards.jsx
@@ -1,3 +1,5 @@
+import { colors } from '@/utils/style/colors';
+import Image from 'next/image';
 import styled from 'styled-components';
 
 export default function LargeCards() {
@@ -5,6 +7,13 @@ export default function LargeCards() {
       <>
          <CardsSection>
             <h1>MON PROFIL</h1>
+            <BackgroundOverlayDiv />
+            <Image
+               src="/../public/images/7603.jpg"
+               alt="background"
+               width="4896"
+               height="3264"
+            />
          </CardsSection>
       </>
    );
@@ -13,10 +22,35 @@ export default function LargeCards() {
 const CardsSection = styled.section`
    width: 100%;
    height: 93vh;
-   padding: 7vw 3.5vw;
-   background-color: beige;
-
-   h1 {
-      margin: 0;
+   padding: 9.5vw 0 0 4vw;
+   position: relative;
+   img {
+      background: linear-gradient(to right, transparent);
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      z-index: 0;
    }
+`;
+
+const BackgroundOverlayDiv = styled.div`
+   position: absolute;
+   top: 0;
+   bottom: 0;
+   left: 0;
+   right: 0;
+   width: 75%;
+   height: 100%;
+   background: linear-gradient(
+      to right,
+      ${colors.appBackground},
+      60%,
+      transparent
+   );
+   z-index: 1;
 `;
