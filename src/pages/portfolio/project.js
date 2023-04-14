f259d4d07c0502251ea285b5021f540f56bcffa7
LionelVanS <lionelvs@outlook.fr>
2023-04-14 18:41:14 +0200

    Creation of project's page

---
+++ src/pages/portfolio/project.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Portfolio() {
+   return (
+      <>
+         <Head>
+            <title>Prime Van S: Portfolio</title>
+            <meta name="description" content="Page d'un projet" />
+         </Head>
+         <main>
+            <h1>Page d'un projet</h1>
+         </main>
+      </>
+   );
+}
