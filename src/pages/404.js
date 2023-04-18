c2c7ea8fc6e47b5a9f093e41865c3f3e9d2d40fd
LionelVanS <lionelvs@outlook.fr>
2023-04-18 14:24:08 +0200

    Updating page 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,27 @@
+import Logo from '@/components/Header/Logo/Logo';
+import Link from 'next/link';
+import styled from 'styled-components';
+
 export default function Error404() {
    return (
-      <>
-         <h1 style={{ textAlign: 'center' }}>Erreur 404 - Page non trouvée</h1>
-      </>
+      <ErrorSection>
+         <h1>Oups ! La page demandé n&apos;existe pas</h1>
+         <Link href="/home">Revenir à la page d&apos;accueil</Link>
+      </ErrorSection>
    );
 }
+
+const ErrorSection = styled.section`
+   height: 70vh;
+   width: 100vw;
+   display: flex;
+   flex-flow: column;
+   justify-content: center;
+   align-items: center;
+   h1 {
+      margin-bottom: 1vw;
+   }
+   a:hover {
+      text-decoration: underline;
+   }
+`;
