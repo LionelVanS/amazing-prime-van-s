c137a5cc60afcd557e672b89a96ff01773214095
LionelVanS <lionelvs@outlook.fr>
2023-04-18 14:33:08 +0200

    Adding large card context

--- src/components/Body/LargeCards/Social/Social.jsx
+++ src/components/Body/LargeCards/Social/Social.jsx
@@ -1,31 +1,30 @@
 import styled from 'styled-components';
 import { useState } from 'react';
 
-import TwitterIcon from '@/components/Icons/TwitterIcon';
-import LinkedInIcon from '@/components/Icons/LinkedInIcon';
-import GithubIcon from '@/components/Icons/GithubIcon';
-import DownloadIcon from '@/components/Icons/DownloadIcon';
 import SocialLink from './SocialLink/SocialLink';
 import LinkDescription from './LinkDescription/LinkDescription';
+import { useLargeCard } from '@/utils/context/largeCard';
 
 export default function Social() {
    // Contains socialLinks.name of hovered icon
    const [isHovered, setIsHovered] = useState();
+
+   const data = useLargeCard();
+   const links = data.links;
+
    return (
       <>
          <SocialDiv>
             <ul>
-               {socialLinks.map((link, index) => (
+               {links.map((link, index) => (
                   <li
                      key={index}
                      onMouseOver={() => setIsHovered(link.name)}
                      onMouseLeave={() => setIsHovered()}
                   >
                      <SocialLink link={link} />
-                     {isHovered == link.name ? (
+                     {isHovered == link.name && (
                         <LinkDescription name={link.name} />
-                     ) : (
-                        ''
                      )}
                   </li>
                ))}
@@ -45,26 +44,3 @@ const SocialDiv = styled.div`
       justify-content: center;
    }
 `;
-
-const socialLinks = [
-   {
-      name: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/LionelVanS',
-      icon: <LinkedInIcon />,
-   },
-   {
-      name: 'Twitter',
-      url: 'https://twitter.com/Lionel_Van_S',
-      icon: <TwitterIcon />,
-   },
-   {
-      name: 'Github',
-      url: 'https://github.com/LionelVanS',
-      icon: <GithubIcon />,
-   },
-   {
-      name: 'Télécharger mon CV',
-      url: '/pdf/CV_Van_Schellebeck_Lionel_DéveloppeurWeb.pdf',
-      icon: <DownloadIcon />,
-   },
-];
