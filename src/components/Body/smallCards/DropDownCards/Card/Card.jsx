9feac46fe737c69bd5d1fe2e424b6e249ca6adef
LionelVanS <lionelvs@outlook.fr>
2023-04-18 14:35:39 +0200

    Removing useEffect to check if it's external link

--- src/components/Body/smallCards/DropDownCards/Card/Card.jsx
+++ src/components/Body/smallCards/DropDownCards/Card/Card.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import DropDown from './DropDown/DropDown';
 import CardLink from './CardLink/CardLink';
 
@@ -10,34 +10,16 @@ export default function Card({ data }) {
    // Boolean to know which card is hovered and which is not
    const isHoveredCard = openingCard == data.title;
 
-   // Check if the data.link is an external link
-   const [isExternalLink, setIsExternalLink] = useState();
-   useEffect(() => {
-      if (data.link.includes('http')) {
-         setIsExternalLink(true);
-      } else {
-         setIsExternalLink(false);
-      }
-   }, [data.link]);
-
    return (
       <ContentArticle
          onMouseOver={() => setOpeningCard(data.title)}
          onMouseOut={() => setOpeningCard(' ')}
          isHoveredCard={isHoveredCard}
       >
-         <CardLink
-            data={data}
-            isHoveredCard={isHoveredCard}
-            isExternalLink={isExternalLink}
-         />
+         <CardLink data={data} isHoveredCard={isHoveredCard} />
          {
             // Drop the card which is hovered
-            isHoveredCard ? (
-               <DropDown data={data} isExternalLink={isExternalLink} />
-            ) : (
-               ''
-            )
+            isHoveredCard ? <DropDown data={data} /> : ''
          }
       </ContentArticle>
    );
