0b5b77ef7d52d0b6adb03208aff95b60e8781938
LionelVanS <lionelvs@outlook.fr>
2023-04-20 12:58:05 +0200

    Adding temporary landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,45 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { handleClick } from '@/utils/functions/handleClick';
 
 export default function LandingPage() {
+   const router = useRouter();
+
    return (
       <>
          <Head>
             <title>Prime Van S</title>
             <meta
                name="description"
-               content="Bienvenue sur mon portfolio. Il s'agit d'un clône du site prime vidéo."
+               content="Bienvenue sur mon portfolio. Il s'agit d'un clône du site d'Amazon Prime Vidéo."
             />
          </Head>
          <main>
-            <div>
-               <h1 style={{ textAlign: 'center' }}>LANDING PAGE</h1>
-            </div>
+            <StyledSection>
+               <h1>Lionel VAN SCHELLEBECK</h1>
+               <h2>Développeur Web React</h2>
+
+               <Link
+                  href="/home"
+                  onClick={(e) => handleClick(e, router, { link: '/' })}
+               >
+                  Entrez
+               </Link>
+            </StyledSection>
          </main>
       </>
    );
 }
+
+const StyledSection = styled.section`
+   height: 50vh;
+   padding: 3vw;
+   display: flex;
+   flex-flow: column;
+   align-items: center;
+   justify-content: center;
+   overflow-x: hidden;
+`;
