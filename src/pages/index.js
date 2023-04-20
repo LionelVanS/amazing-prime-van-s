e223074994131a5dee75e20ffd7207da334a815b
LionelVanS <lionelvs@outlook.fr>
2023-04-20 18:43:22 +0200

    Updating landing page and functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,29 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
 import { handleClick } from '@/utils/functions/handleClick';
+import { useEffect, useState } from 'react';
+import { converttextToLetters } from '@/utils/functions/convertTextToLetters';
+import { motion } from 'framer-motion';
+import whiteRabbit from '../../public/images/white-rabbit.png';
+import Image from 'next/image';
+import { animatedSentences } from '@/utils/functions/getAnimatedSentences';
 
 export default function LandingPage() {
    const router = useRouter();
+   const [wakeUpLetters, setWakeUpLetters] = useState();
+   const [matrixLetters, setMatrixLetters] = useState();
+   const [rabbitLetters, setRabbitLetters] = useState();
+
+   const text = 'Wake up, Recruter...';
+   const title = 'The Matrix has you...';
+   const rabbit = 'Follow the White Rabbit.';
+
+   useEffect(() => {
+      setWakeUpLetters(converttextToLetters(text));
+      setMatrixLetters(converttextToLetters(title));
+      setRabbitLetters(converttextToLetters(rabbit));
+   }, [text, title, rabbit]);
 
    return (
       <>
@@ -19,15 +37,62 @@ export default function LandingPage() {
          </Head>
          <main>
             <StyledSection>
-               <h1>Lionel VAN SCHELLEBECK</h1>
-               <h2>Développeur Web React</h2>
-
-               <Link
-                  href="/home"
-                  onClick={(e) => handleClick(e, router, { link: '/' })}
+               <TextDiv>
+                  <AnimateP>
+                     {wakeUpLetters &&
+                        wakeUpLetters.map((letter, index) =>
+                           animatedSentences(
+                              motion,
+                              wakeUpLetters,
+                              matrixLetters,
+                              letter,
+                              index,
+                              (parent = 'wakeUpText')
+                           )
+                        )}
+                  </AnimateP>
+                  <AnimateP>
+                     {matrixLetters &&
+                        matrixLetters.map((letter, index) =>
+                           animatedSentences(
+                              motion,
+                              wakeUpLetters,
+                              matrixLetters,
+                              letter,
+                              index,
+                              (parent = 'matrixText')
+                           )
+                        )}
+                  </AnimateP>
+                  <AnimateP>
+                     {rabbitLetters &&
+                        rabbitLetters.map((letter, index) =>
+                           animatedSentences(
+                              motion,
+                              wakeUpLetters,
+                              matrixLetters,
+                              letter,
+                              index,
+                              (parent = 'rabbitText')
+                           )
+                        )}
+                  </AnimateP>
+               </TextDiv>
+               <StyledLinkDiv
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 9 }}
                >
-                  Entrez
-               </Link>
+                  <Link
+                     href="/home"
+                     onClick={(e) => handleClick(e, router, { link: '/' })}
+                  >
+                     <Image
+                        src={whiteRabbit}
+                        alt="Visitez mon site portfolio"
+                     />
+                  </Link>
+               </StyledLinkDiv>
             </StyledSection>
          </main>
       </>
@@ -35,11 +100,30 @@ export default function LandingPage() {
 }
 
 const StyledSection = styled.section`
-   height: 50vh;
+   height: 100vh;
    padding: 3vw;
    display: flex;
    flex-flow: column;
-   align-items: center;
+   justify-content: space-between;
+`;
+
+const AnimateP = styled.p`
+   font-family: 'Courier Prime', sans-serif;
+   font-size: 1.5vw;
+   font-weight: 600;
+   margin: 1vw 0;
+   text-shadow: 0px 0px 9px rgba(120, 196, 117, 1);
+`;
+const TextDiv = styled.div``;
+const StyledLinkDiv = styled(motion.div)`
+   display: flex;
    justify-content: center;
-   overflow-x: hidden;
+   img {
+      width: 4vw;
+      height: 4vw;
+      transition: transform 100ms ease-in-out;
+      :hover {
+         transform: scale(1.2);
+      }
+   }
 `;
---
+++ src/utils/functions/convertTextToLetters.js
@@ -0,0 +1,5 @@
+export function converttextToLetters(text) {
+   if (text.length === 0) return;
+
+   return text.split('');
+}
