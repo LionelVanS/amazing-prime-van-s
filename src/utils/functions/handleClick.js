f735a97c9db6371329d95d6f52fa96cfbe6c93ad
LionelVanS <lionelvs@outlook.fr>
2023-04-25 17:40:12 +0200

    Adding verification to catch pdf case

--- src/utils/functions/handleClick.js
+++ src/utils/functions/handleClick.js
@@ -6,9 +6,10 @@ export function handleClick(e, router, data) {
          router.push(`/portfolio/${data.id}`);
          break;
 
-      case data.link.includes('http'):
+      case data.link.includes('http') || data.link.includes('pdf'):
          window.open(data.link, '_blank', 'noopener,noreferrer');
          break;
+
       default:
          router.push(data.link);
    }
