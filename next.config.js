f14cfbd9fb0ed2a4fc27886654776fb1fa9300b6
LionelVanS <lionelvs@outlook.fr>
2023-04-25 17:30:05 +0200

    Adding config to handle pdf file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    reactStrictMode: true,
-   compiler: {
-      styledComponents: true,
+   webpack: (config) => {
+      config.module.rules.push({
+         test: /\.pdf$/,
+         use: {
+            loader: 'file-loader',
+            options: {
+               name: '[path][name].[ext]',
+            },
+         },
+      });
+      return config;
    },
 };
-
 module.exports = nextConfig;
