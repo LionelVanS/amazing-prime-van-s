e0db7885c2c5ff0bf55423ed65467c328e4da619
LionelVanS <lionelvs@outlook.fr>
2023-04-26 11:37:49 +0200

    Updating transform origin of cards close to the edge

--- src/components/Body/smallCards/DropDownCards/Card/Card.jsx
+++ src/components/Body/smallCards/DropDownCards/Card/Card.jsx
@@ -1,36 +1,48 @@
 import styled from 'styled-components';
 import { useState } from 'react';
+import { handleHover } from '@/utils/functions/handleHover';
 import DropDown from './DropDown/DropDown';
 import CardLink from './CardLink/CardLink';
 
 export default function Card({ data }) {
    // Contains data.title of hovered card
-   const [openingCard, setOpeningCard] = useState(' ');
+   const [openedCard, setOpenedCard] = useState(' ');
+
+   // Contain string if card is too close to edge from right or left
+   const [tooClose, setTooClose] = useState('');
 
    // Boolean to know which card is hovered and which is not
-   const isHoveredCard = openingCard == data.title;
+   const isHoveredCard = openedCard == data.title;
 
    return (
-      <ContentArticle
-         onMouseOver={() => setOpeningCard(data.title)}
-         onMouseOut={() => setOpeningCard(' ')}
+      <CardArticle
+         onMouseOver={(e) => handleHover(e, data, setOpenedCard, setTooClose)}
+         onMouseOut={(e) => handleHover(e, data, setOpenedCard, setTooClose)}
          isHoveredCard={isHoveredCard}
+         tooClose={tooClose}
       >
          <CardLink data={data} isHoveredCard={isHoveredCard} />
+
          {
-            // Drop the card which is hovered
-            isHoveredCard ? <DropDown data={data} /> : ''
+            // Open the card which is hovered
+            isHoveredCard && <DropDown data={data} />
          }
-      </ContentArticle>
+      </CardArticle>
    );
 }
 
-const ContentArticle = styled.article`
+const CardArticle = styled.article`
    width: 18vw;
    height: 10vw;
    margin: 0 0.5vw;
    position: relative;
    z-index: ${(props) => (props.isHoveredCard ? 2 : 1)};
+   transform-origin: ${(props) =>
+      props.tooClose == 'left'
+         ? 'left'
+         : props.tooClose == 'right'
+         ? 'right'
+         : 'center'};
    :hover {
       box-shadow: 0px 0px 21px rgba(0, 0, 0, 0.61);
       transition: transform 100ms 150ms ease-in;
