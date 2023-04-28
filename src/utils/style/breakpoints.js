7aef2a86e8d1b35fd816cf0994aa06eff5f74c0c
LionelVanS <lionelvs@outlook.fr>
2023-04-28 17:32:30 +0200

    Updating breakpoints

--- src/utils/style/breakpoints.js
+++ src/utils/style/breakpoints.js
@@ -1,13 +1,16 @@
-const sizes = {
+export const sizes = {
    mobileM: '448px',
    mobileL: '880px',
-   tablet: '1400px',
+   tabletMin: '881px',
+   tabletMax: '1399px',
+   desktop: '1400px',
    desktopL: '1800px',
 };
 
 export const devices = {
    mobileM: `(max-width: ${sizes.mobileM})`,
    mobileL: `(max-width: ${sizes.mobileL})`,
-   tablet: `(max-width: ${sizes.tablet})`,
+   tablet: `(min-width: ${sizes.tabletMin}) and (max-width: ${sizes.tabletMax})`,
+   desktop: `(min-width: ${sizes.desktop})`,
    desktopL: `(min-width: ${sizes.desktopL})`,
 };
