19e6440328696d4ee122f91d14ef9dfe89885ee7
LionelVanS <lionelvs@outlook.fr>
2023-05-07 14:09:52 +0200

    Adding functions

---
+++ src/utils/functions/handleUserMenuEvent.js
@@ -0,0 +1,36 @@
+export function handleUserMenuEvent(e, userMenuProps) {
+   const {
+      screenWidth,
+      sizes,
+      setUserMenuIsHovered,
+      setUserMenuIsClicked,
+      userMenuIsClicked,
+      userMenuIsHovered,
+   } = userMenuProps;
+
+   const type = e.type;
+   const mobile = screenWidth <= sizes.mobileL;
+   const tabletScreenSize =
+      screenWidth <= sizes.tabletMax && screenWidth >= sizes.tabletMin;
+   const desktop = screenWidth >= sizes.desktop;
+   const tabletDevice =
+      /iPad|Android/.test(navigator.userAgent) && !window.MSStream;
+
+   switch (true) {
+      case type == ('mouseenter' || 'mouseleave') && (mobile || tabletDevice):
+         return;
+         break;
+      case type == 'mouseenter' && (tabletScreenSize || desktop):
+         setUserMenuIsHovered(!userMenuIsHovered);
+         break;
+      case type == 'mouseleave' && (tabletScreenSize || desktop):
+         setUserMenuIsHovered(!userMenuIsHovered);
+         break;
+      case type == 'click' && (mobile || tabletDevice):
+         setUserMenuIsClicked(!userMenuIsClicked);
+
+         break;
+      default:
+         return;
+   }
+}
